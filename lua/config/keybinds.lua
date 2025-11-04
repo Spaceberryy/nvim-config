aa70ad17520ae208e1450a79a94a832218322d0b
Spaceberryy <haadynaushahi7@gmail.com>
2025-11-04 12:34:52 +0500

    Added onemore to config

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,11 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
+vim.opt.virtualedit = "onemore"
+
 vim.g.mapleader = " "
 
 local keymap = vim.keymap.set
 
 keymap("i", "J", "<Esc>") -- exit insert with J
-keymap("n", "L", "$")     -- end of line
+keymap("n", "L", "$l", { noremap = true, silent = true })
 keymap("n", "H", "^")     -- beginning of line
 keymap("n", "<Esc>", ":")

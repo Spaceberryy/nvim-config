d111958a2f5eecb6652971a137d10ed32cd6148b
spaceberry <haadynaushahi7@gmail.com>
2026-03-30 15:09:50 +0500

    removing onemore to shift L

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -8,6 +8,8 @@ vim.g.mapleader = " "
 local keymap = vim.keymap.set
 
 keymap("i", "J", "<Esc>") -- exit insert with J
-keymap("n", "L", "$l", { noremap = true, silent = true })
+keymap("n", "L", "$", { noremap = true, silent = true })
 keymap("n", "H", "^")     -- beginning of line
+keymap("v", "L", "$", { noremap = true, silent = true })
+keymap("v", "H", "^")     -- beginning of line
 keymap("n", "<Esc>", ":")

9470ccf0d2d6a3f0026adddc5b370db300414355
spaceberry <haadynaushahi7@gmail.com>
2026-08-12 21:49:47 +0500

    modified colorschemes file to work with treesitter

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,33 +1,69 @@
 local function enable_transparency()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- transparent floats
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
+    -- Catppuccin
     {
-        "folke/tokyonight.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         lazy = false,
         opts = {
-            style = "moon",     -- try "moon" or "storm" for better float contrast
-            transparent = true, -- still need this false to apply background highlight tweaks
-            terminal_colors = true,
-            styles = {
-                floats = "transparent", -- affects ToggleTerm and floating windows
-                sidebars = "transparent",
+            flavour = "mocha",
+            transparent_background = true,
+            integrations = {
+                lualine = true,
+                treesitter = true,
+                telescope = true,
+                nvimtree = true,
+                which_key = true,
+                gitsigns = true,
             },
         },
         config = function(_, opts)
-            require("tokyonight").setup(opts)
-            vim.cmd.colorscheme("tokyonight-moon")
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme("catppuccin-mocha")
             enable_transparency()
         end,
     },
+
+    -- TokyoNight
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = {
+            style = "moon",
+            transparent = true,
+            terminal_colors = true,
+            styles = {
+                floats = "transparent",
+                sidebars = "transparent",
+            },
+        },
+    },
+
+    -- Lualine
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
-            theme = "tokyonight",
+            theme = "catppuccin",
+        },
+    },
+
+    -- Colorscheme picker
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            {
+                "<leader>tc",
+                function()
+                    require("telescope.builtin").colorscheme()
+                end,
+                desc = "Colorschemes",
+            },
         },
     },
 }
